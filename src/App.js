137f508199c242fd8bb8dd604c559c0b99f1f432
SebasProgrammer2020 <sebastian.londono@atlanticsoft.us>
2022-05-14 14:39:11 -0500

    added layout and tailwind

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./modules/home/Home";
 import About from "./modules/about/About";
-// test new branch
+import Layout from "./components/layout/Layout";
+
 function App() {
   return (
-    <div className="App">
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
